86469e99488da2a194ed13ce649afa88e265b655
Ramkumar Ramachandra <artagnon@gmail.com>
2019-01-29 21:58:05 +0530

    [claytext.js] Handle <a> with ease

--- design/claytext.js
+++ design/claytext.js
@@ -1,6 +1,7 @@
 $(function() {
-  var paragraphs = document.getElementsByTagName("p");
-  for (i = 0; i < paragraphs.length; ++i) {
-    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(/`([^`]+)`/g, "<span class=\"tick\">$1</span>");
+  var paras = document.getElementsByTagName("p");
+  for (i = 0; i < paras.length; ++i) {
+    paras[i].innerHTML = paras[i].innerHTML.replace(/`([^`]+)`/g, "<span class=\"tick\">$1</span>");
+    paras[i].innerHTML = paras[i].innerHTML.replace(/\[([^\]]+)\]\(([^\)]+)\)/g, "<a href=\"$2\" target=\"_blank\">$1</a>")
   }
 })
