604d88f6b6a5fa3450de4e15baf7dd21636b5ea5
Ramkumar Ramachandra <artagnon@gmail.com>
2019-02-01 19:04:31 +0530

    [claytext.js] jQuer'ify

--- design/claytext.js
+++ design/claytext.js
@@ -1,7 +1,6 @@
 $(function() {
-  var paras = document.getElementsByTagName("p");
-  for (i = 0; i < paras.length; ++i) {
-    paras[i].innerHTML = paras[i].innerHTML.replace(/`([^`]+)`/g, "<span class=\"tick\">$1</span>");
-    paras[i].innerHTML = paras[i].innerHTML.replace(/\[([^\]]+)\]\(([^\)]+)\)/g, "<a href=\"$2\" target=\"_blank\">$1</a>")
-  }
-})
+  $("p, li").each(function() {
+    $(this).html($(this).html().replace(/`([^`]+)`/g, "<span class=\"tick\">$1</span>"));
+    $(this).html($(this).html().replace(/\[([^\]]+)\]\(([^\)]+)\)/g, "<a href=\"$2\" target=\"_blank\">$1</a>"));
+  });
+});
