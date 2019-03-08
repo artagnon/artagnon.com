43ef5a4f402863a4acf42480af1ac5efe1436d05
Ramkumar Ramachandra <artagnon@gmail.com>
2019-03-08 14:21:43 +0530

    [BingSiteAuth, pingsitemap] Some SEO

---
+++ pingsitemap.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/fish
+
+wget -O - "http://www.google.com/webmasters/tools/ping?sitemap=https%3A%2F%2Fartagnon.com%2Fsitemap.xml.gz"
+wget -O - "http://www.bing.com/ping?sitemap=https%3A%2F%2Fartagnon.com%2Fsitemap.xml.gz"
+
