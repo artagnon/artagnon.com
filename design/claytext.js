51255ad761dc2426c8e053aa8b66c9b14526cace
Ramkumar Ramachandra <artagnon@gmail.com>
2019-12-13 21:20:13 +0100

    [js] Focus on #main on startup

--- design/claytext.js
+++ design/claytext.js
@@ -1,6 +1,15 @@
 $(function() {
+  window.location.hash = "#main";
   $("p, li").each(function() {
-    $(this).html($(this).html().replace(/`([^`]+)`/g, "<span class=\"tick\">$1</span>"));
-    $(this).html($(this).html().replace(/\[([^\]]+)\]\(([^\)]+)\)/g, "<a href=\"$2\">$1</a>"));
+    $(this).html(
+      $(this)
+        .html()
+        .replace(/`([^`]+)`/g, '<span class="tick">$1</span>')
+    );
+    $(this).html(
+      $(this)
+        .html()
+        .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>')
+    );
   });
 });
