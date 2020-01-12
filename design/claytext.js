d724d3401a953a0baaa072884d49ebc2ed88695a
Ramkumar Ramachandra <artagnon@gmail.com>
2020-01-12 14:27:21 +0100

    [design] moment'ize lastmod timestamps

--- design/claytext.js
+++ design/claytext.js
@@ -1,15 +1,23 @@
 $(function() {
   document.getElementById("main").focus();
+
   $("p, li").each(function() {
+    // `...` text
     $(this).html(
       $(this)
         .html()
         .replace(/`([^`]+)`/g, '<span class="tick">$1</span>')
     );
+    // [...](...) links
     $(this).html(
       $(this)
         .html()
         .replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>')
     );
+  });
+
+  // humanize timestamp
+  $("#timestamp time").each(function() {
+    $(this).text(moment($(this).text()).fromNow());
   });
 });
