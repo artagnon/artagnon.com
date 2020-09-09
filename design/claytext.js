fe0f0180fa18995fef0b83ef2b383237d3426d57
Ramkumar Ramachandra <artagnon@gmail.com>
2020-09-09 18:20:12 +0200

    [Regen] Move js regexes for <{mark, a}> to clayoven

--- design/claytext.js
+++ design/claytext.js
@@ -3,18 +3,6 @@ $(() => {
   // main should have a valid taborder
   document.querySelector("main").focus();
 
-  for (el of $("p, li")) {
-    // we use fn.html() from cash
-    $(el).html(
-      $(el)
-        .html()
-        // `...` text
-        .replace(/`([^`]+)`/g, "<mark>$1</mark>")
-        // [...](...) links
-        .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>')
-    );
-  }
-
   // humanize timestamp
   dayjs.extend(window.dayjs_plugin_relativeTime);
   for (el of $("#timestamp time")) {
